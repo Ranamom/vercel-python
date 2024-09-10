20224c2c8f56f0881f933a7888e1fce833a34a80
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-09-10 19:03:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,19 +1,16 @@
-BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
 from urllib import parse
 
-class handler(BaseHTTPRequestHandler):
+def handler(req, res):
+    s = req.url
+    dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    res.status_code = 200
+    res.headers['Content-Type'] = 'text/plain'
 
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
+    if "name" in dic:
+        message = "Hello, " + dic["name"] + "!"
+    else:
+        message = "Hello, stranger!"
 
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
-		return
+    res.body = message
+    return res
